2d27a5f30f53a9cd8f15ea12a8a86fce4229852e
Randy Runtsch <rruntsch@gmail.com>
2021-08-16 13:48:37 -0500

    Add files via upload

--- get_bls_cpi_data.py
+++ get_bls_cpi_data.py
@@ -9,7 +9,7 @@ print("Program started.")
 # Set the register, series ID for CPI, start year, end year, and calculations. Note that setting calculations to true
 # returns CPI percentages as well as the raw CPI.
 
-parameters = json.dumps({"registrationkey":"0cc525c07f824fb2946765a8e18a18a7", "seriesid":['CUUR0000SA0'], "startyear":"2002", "endyear":"2021", "calculations":"true"})
+parameters = json.dumps({"registrationkey":"PasteYourKeyHere", "seriesid":['CUUR0000SA0'], "startyear":"2002", "endyear":"2021", "calculations":"true"})
 
 # Call the bls data api class with the parameters and the name of the data output file.
 c_bls_data_api(parameters, 'D:/project_data/cpi/cpi_data_report.json')
